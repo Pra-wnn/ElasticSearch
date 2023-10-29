b18b12864e10dc2645c733a96af2fbdc9f517929
Praween Chhetri <62017020+Pra-wnn@users.noreply.github.com>
2023-10-29 17:36:30 +0545

    Update loader.py

--- ElasticSearch/loader.py
+++ ElasticSearch/loader.py
@@ -1,22 +1,3 @@
-# from elasticsearch import Elasticsearch
-
-# import csv
-
-# es = Elasticsearch(hosts=["http://127.0.0.1:9200"])
-
-# print(f"Connected to ElasticSearch cluster `{es.info().body['cluster_name']}`")
-
-# with open("./Car details v3.csv", "r") as f:
-#     reader = csv.reader(f)
-
-#     for i, line in enumerate(reader):
-#         document = {
-#             "name": line[0],
-#             "engine": line[9],
-#             "year": line[1],
-#             "price": line[2],
-#         }
-#         es.index(index="cars", document=document)
 
 from elasticsearch import Elasticsearch
 from elasticsearch import Elasticsearch
@@ -68,6 +49,7 @@ es = Elasticsearch(hosts=["http://127.0.0.1:9200"])
 print(f"Connected to ElasticSearch cluster `{es.info().body['cluster_name']}`")
 
 with open("./data.txt", "r",encoding="utf-8") as f:
+    #Change to your specific filename and path
     lines = f.readlines()
 
     for i, line in enumerate(lines):
